acf93a25bb03f6337a6b7dc0ce3ee446d917bdfd
Daniilka48 <WellDevil48@yandex.ru>
2025-05-05 12:48:29 +0300

    test husky

--- src/comp.ts
+++ src/comp.ts
@@ -1,9 +1,1 @@
-const message = 'Hello world!'
-const add = (a:number,b:number) =>{
-    return a+b
-}
-add(5,10)
-
-const obj = {firstName: "John",lastName:"Doe", age: 30}
-
-if(message === 'Hello world!') { console.log('Hello!')}
+const message = "5";
