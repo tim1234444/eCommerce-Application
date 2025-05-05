bf5bfea5fd252e267f64499e3071892855e6552e
Daniilka48 <WellDevil48@yandex.ru>
2025-05-05 12:43:46 +0300

    test husky

--- src/comp.ts
+++ src/comp.ts
@@ -1,1 +1,9 @@
-const x = "4";
+const message = 'Hello world!'
+const add = (a:number,b:number) =>{
+    return a+b
+}
+add(5,10)
+
+const obj = {firstName: "John",lastName:"Doe", age: 30}
+
+if(message === 'Hello world!') { console.log('Hello!')}
