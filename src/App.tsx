8554deb1b76098a4bcb46fd42858393a303d276d
1 <tim1234444>
2025-05-19 21:00:42 +0300

    create loginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import './App.css';
+import { RedirectIfAuth } from './components/RedirectIfAuth';
+
+import LoginPage from './pages/loginPage';
 import MainPage from './pages/mainPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/autorisation" element={<div></div>} />
-        <Route path="/" element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
+  <Routes>
+    <Route
+      path="/autorisation"
+      element={
+        <RedirectIfAuth>
+          <LoginPage />
+        </RedirectIfAuth>
+      }
+    />
+    <Route path="/" element={<MainPage />} />
+  </Routes>
+</BrowserRouter>
   );
 }
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,38 @@
-import type { ReactNode } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState, type ReactNode } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 interface IChildren {
   children: ReactNode;
 }
-
+const isLogin = localStorage.getItem('access_token');
+console.log(isLogin)
 export default function Layout({ children }: IChildren) {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    setIsLogin(!!token);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('access_token');
+    setIsLogin(false);
+    navigate('/autorisation');
+  };
+
   return (
     <>
       <header>
         <nav>
+          {isLogin && <button onClick={handleLogout}>Выйти</button>}
           <ul>
+            {!isLogin && (
+              <li>
+                <Link to="/autorisation">Authorization</Link>
+              </li>
+            )}
             <li>
-              <Link to="/one">Page One</Link>
-            </li>
-            <li>
-              <Link to="/two">Page Two</Link>
+              <Link to="/">Main</Link>
             </li>
           </ul>
         </nav>
